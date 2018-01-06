0b88802012691bff83f1e21f9b017e121616257f
Brandon Stewart <boveus@gmail.com>
2018-01-06 16:22:41 -0700

    Updates

--- src/components/helpers/random-color.js
+++
@@ -1,9 +0,0 @@
-export { getRandomRgb }
-
-function getRandomRgb (randomness) {
-  let num = Math.round(0xffffff * Math.random()) * randomness
-  let r = num >> 16
-  let g = num >> 8 & 255
-  let b = num & 255
-  return `rgb(${r},${g},${b})`
-}
